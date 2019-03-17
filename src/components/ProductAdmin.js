6a31981b6601251f3d9232273f427a0cf4701e81
Jonathan Spruance <jgspruance@gmail.com>
2019-03-16 18:22:41 -0700

    Correct id param in request urls

--- src/components/ProductAdmin.js
+++ src/components/ProductAdmin.js
@@ -21,7 +21,7 @@ export default class ProductAdmin extends Component {
         "id": id,
         "productname": this.state.newproduct.productname
       };
-      await axios.post(`${config.api.invokeUrl}/products/{id}`, params);
+      await axios.post(`${config.api.invokeUrl}/products/${id}`, params);
       this.setState({ products: [...this.state.products, this.state.newproduct] });
       this.setState({ newproduct: { "productname": "", "id": "" }});
     }catch (err) {
@@ -36,7 +36,7 @@ export default class ProductAdmin extends Component {
         "id": id,
         "productname": name
       };
-      await axios.patch(`${config.api.invokeUrl}/products/{id}`, params);
+      await axios.patch(`${config.api.invokeUrl}/products/${id}`, params);
       const productToUpdate = [...this.state.products].find(product => product.id === id);
       const updatedProducts = [...this.state.products].filter(product => product.id !== id);
       productToUpdate.productname = name;
@@ -51,7 +51,7 @@ export default class ProductAdmin extends Component {
     event.preventDefault();
     // add call to AWS API Gateway delete product endpoint here
     try {
-      await axios.delete(`${config.api.invokeUrl}/products/{id}`);
+      await axios.delete(`${config.api.invokeUrl}/products/${id}`);
       const updatedProducts = [...this.state.products].filter(product => product.id !== id);
       this.setState({products: updatedProducts});
     }catch (err) {
